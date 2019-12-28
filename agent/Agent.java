f81166137e6e953bbbe5f2668223746aca926552
Max Chatfield <maxc7.08@gmail.com>
2019-12-28 19:07:44 +0800

    agent package and template classes

---
+++ agent/Agent.java
@@ -0,0 +1,9 @@
+package catan.agent;
+
+import java.awt.Color;
+
+// agent interface
+public interface Agent
+{
+	public Color get_color();
+}
---
+++ agent/RandomAgent.java
@@ -0,0 +1,12 @@
+package catan.agent;
+
+import java.awt.Color;
+
+// randomly does moves
+public class RandomAgent extends Agent
+{
+	public RandomAgent(Color color)
+	{
+		super(color);
+	}
+}
