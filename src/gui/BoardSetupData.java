c4d8f27073771093844d33ed416ea4de3d0293e3
Max Chatfield <maxc7.08@gmail.com>
2020-01-24 18:36:05 +0800

    struct class to consolidate data about board generation

---
+++ src/gui/BoardSetupData.java
@@ -0,0 +1,19 @@
+package src.gui;
+
+// a struct type class for storing data relevant to board setup
+// for use in generation and selecting manaully
+// regular_game - whether is 3-catan or 4-ext
+// rule6_8 - whether the 6,8 token rule should be enforced
+public class BoardSetupData
+{
+	public boolean regular_game;
+	
+	public boolean rule6_8;
+	
+	public String toString()
+	{
+		String str = regular_game + " " + rule6_8;
+		
+		return str;
+	}
+}
