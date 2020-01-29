940aee06bca6695aeafb22180906d4f10735dd71
Max Chatfield <maxc7.08@gmail.com>
2020-01-29 14:05:39 +0800

    now has port info

--- src/game/Edge.java
+++ src/game/Edge.java
@@ -9,6 +9,11 @@ public class Edge
 	// -1 for nothing, 0 for road
 	private int type;
 	
+	// port info for neighbouring vertices to use
+	// makes more sense to tie this data to edges
+	// -1 nothing, 0 - wood, 1 - brick, 2 - sheep, 3 - wheat, 4 - ore, 5 - any
+	private int port;
+	
 	// array indices
 	private int i, j;
 	
@@ -19,6 +24,7 @@ public class Edge
 	{
 		this.player = -1;
 		this.type = -1;
+		this.port = -1;
 		i = j = -1;
 		
 		node_edge = null;
@@ -28,16 +34,33 @@ public class Edge
 	{
 		this.player = -1;
 		this.type = -1;
+		this.port = -1;
 		this.i = i;
 		this.j = j;
 		
 		node_edge = null;
 	}
 
-	public void set(int player, int type)
+	public void set_player(int player)
+	{
+		this.player = player;
+	}
+	
+	public void set_type(int type)
+	{
+		this.type = type;
+	}
+	
+	public void set_port(int port)
+	{
+		this.port = port;
+	}
+	
+	public void set(int player, int type, int port)
 	{
 		this.player = player;
 		this.type = type;
+		this.port = port;
 	}
 	
 	public String toString()
@@ -53,6 +76,11 @@ public class Edge
 	public int get_type()
 	{
 		return type;
+	}
+	
+	public int get_port()
+	{
+		return port;
 	}
 	
 	// get i and j in 
