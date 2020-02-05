745c4b5436d011f7b41f8bd19de4c2c17ddca3b8
Max Chatfield <maxc7.08@gmail.com>
2020-02-05 14:24:07 +0800

    now initializes agents properly based on their type, also does starting pos

--- src/game/CatanEngine.java
+++ src/game/CatanEngine.java
@@ -21,6 +21,11 @@ public class CatanEngine implements Catan
 		
 	private Agent[] agents;
 	
+	// starting order of agents
+	// order[0] - pos of agents[0], from 1 to agents.length
+	// if not set current state will be 0
+	private int[] order;
+	
 	private GameData game_data;
 
 	// state of the game
@@ -44,6 +49,18 @@ public class CatanEngine implements Catan
 		this.board = new Board(rng);
 		
 		agents = new Agent[game_data.players_amount];
+		
+		// starting positions have already been decided
+		if (game_data.starting_enabled)
+		{
+			current_state = 1;
+			this.order = game_data.poses;
+		}
+		else
+		{
+			current_state = 0;
+			this.order = new int[agents.length];
+		}
 		
 		setup(game_data.board_size, game_data.game_mode);
 	}
@@ -73,13 +90,28 @@ public class CatanEngine implements Catan
 		
 		board.initialize_nodes_normal();
 		
-		// this will need to change eventually
+		String types[] = game_data.types; // agent types
+		
 		for (int i = 0; i < agents.length; i++)
 		{
-			agents[i] = new HumanAgent(new Color(game_data.colors[i]));
+			if (types[i] == "Human")
+			{
+				agents[i] = new HumanAgent(new Color(game_data.colors[i]), game_data.names[i]);
+			}
+			else if (types[i] == "Random Bot")
+			{
+				agents[i] = new RandomAgent(new Color(game_data.colors[i]), game_data.names[i]);
+			} 
+			// more types to come...........
 		}
 	}
-		
+	
+	// get current state
+	public int get_state()
+	{
+		return current_state;
+	}
+	
 	// get current board
 	public Board get_board()
 	{
@@ -96,10 +128,29 @@ public class CatanEngine implements Catan
 			
 		return colors;
 	}
-		
-	// reset method for testing
-	// if its regular catan 4-player or 6 it will use the standard setups
-	// otherwise it generates randomly using test_randomize_all
+	
+	// generate_ports
+	public void generate_ports(BoardSetupData setup)
+	{
+		if (setup.regular_game)
+		{
+			if (game_data.game_mode == 0)
+			{
+				board.set_ports_reg();
+			}
+			else
+			{
+				board.set_ports_ext();
+			}
+		}
+		else
+		{
+			// future ports algos
+		}
+	}
+	
+	// if its regular 3-catan or 4-ext it will use the standard setups
+	// otherwise it generates randomly using get_tile_setup and token_setup
 	public void generate_board(BoardSetupData setup)
 	{
 		if (setup.regular_game)
@@ -120,6 +171,13 @@ public class CatanEngine implements Catan
 			int token[] = board.get_token_setup(tile_setup[5]);
 			board.set_board(tile_setup, token, setup.rule6_8);
 		}
+		
+		generate_ports(setup);
 	}
-
+	
+	// clears hexes/tokens on this board
+	public void clear_board()
+	{
+		board.clear_board();
+	}
 }
