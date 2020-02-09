b79c64cb1d4189eafbb542877537ede004551772
Max Chatfield <maxc7.08@gmail.com>
2020-02-09 11:52:56 +0800

    update info texts next to each agent, and set agent order

--- src/gui/GameInfoPanel.java
+++ src/gui/GameInfoPanel.java
@@ -7,26 +7,63 @@ import java.awt.Color;
 
 import src.game.GameData;
 
+import src.agent.Agent;
+
 public class GameInfoPanel extends JPanel
 {
     private JLabel title;
+	
     private JLabel players[];
-
+	
+	// additional information to tack on to the end of each label
+	// like current turn, dice roll
+	private JLabel info[];
+	
     public GameInfoPanel(GameData game_data)
     {
         this.setLayout(new GridLayout(0,1));
 		
+		Font font = new Font("Comic Sans MS", Font.BOLD, 20);
+		
         title = new JLabel("Players");
+		title.setFont(font);
+		
 		this.add(title);
 		
         players = new JLabel[game_data.players_amount];
-
+		info = new JLabel[game_data.players_amount];
+		
         for(int i = 0; i < game_data.players_amount; i++)
         {
-			players[i] = new JLabel(game_data.names[i]);
+			JPanel hold = new JPanel(new GridLayout(1,2));
+			
+			players[i] = new JLabel();
+			players[i].setFont(font);
 			players[i].setOpaque(true);
-			players[i].setBackground(new Color(game_data.colors[i]));
-			this.add(players[i]);
+			
+			info[i] = new JLabel("");
+			info[i].setFont(font);
+			
+			hold.add(players[i]);
+			hold.add(info[i]);
+			this.add(hold);
         }
     }
+	
+	// called when the starting order of the players has been decided
+	public void set_order(Agent[] agents)
+	{
+		for (int i = 0; i < players.length; i++)
+		{
+			players[i].setText(agents[i].get_name());
+			players[i].setBackground(agents[i].get_color());
+			info[i].setText("");
+		}
+	}
+	
+	// to update corresponding infos for players
+	public void update_info(int index, String update)
+	{
+		info[index].setText(update);
+	}
 }
