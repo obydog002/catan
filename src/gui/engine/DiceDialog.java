f207129c623102718297dfa2bff4672cf90b1e91
Max Chatfield <maxc7.08@gmail.com>
2020-02-09 11:47:19 +0800

    dice dialog implementation finished, complete with random rolls showing

--- src/gui/engine/DiceDialog.java
+++ src/gui/engine/DiceDialog.java
@@ -14,33 +14,65 @@ public class DiceDialog extends JDialog implements ActionListener
 	// panel for drawing dice stuff
 	private DicePanel dice_panel;
 	
-	public DiceDialog(JFrame parent)
+	public DiceDialog(JFrame parent, int d1, int d2)
 	{
 		super(parent, true);
 		
-		JPanel main_panel = new JPanel(new GridLayout(0,1));
+		// close button do nothing
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		
+		JPanel main_panel = new JPanel(new GridBagLayout());
 		
 		ok = new JButton("okay");
 		ok.addActionListener(this);
+		ok.setEnabled(false); // initially false so we wait for the dice roll to complete
 		
-		dice_panel = new DicePanel();
+		Dimension dim = parent.getSize();
 		
-		main_panel.add(dice_panel);
-		main_panel.add(ok);
+		// a third of parent frame 
+		Dimension third_size = new Dimension((int)dim.getWidth()/3, (int)dim.getHeight()/3);
+		dice_panel = new DicePanel(this, d1, d2, third_size);
 		
-		add(main_panel, BorderLayout.CENTER);
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.weightx = 1.0;
+		c.weighty = 1.0;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 0;
+		c.gridy = 0;
+		
+		// so it always fills available space
+		JPanel buffer = new JPanel(new BorderLayout());
+		buffer.add(dice_panel, BorderLayout.CENTER);
+		
+		main_panel.add(buffer, c);
+		
+		c.weightx = 0.0;
+		c.weighty = 0.0;
+		c.gridy = 1;
+		c.fill = GridBagConstraints.NONE;
+		main_panel.add(ok, c);
+		
+		add(main_panel);
 		
 		this.pack();
 		
 		// location relative to middle of frame
 		Point loc = parent.getLocation();
-		Dimension dim = parent.getSize();
 		
 		Dimension this_size = this.getSize();
 		
 		setLocation(loc.x + (int)(dim.getWidth() - this_size.getWidth())/2, loc.y + (int)(dim.getHeight() - this_size.getHeight())/2);
 		
 		this.setVisible(false);
+	}
+	
+	// called by panel after its done drawing
+	public void enable_ok()
+	{
+		ok.setEnabled(true);
 	}
 	
 	public void actionPerformed(ActionEvent e)
@@ -52,6 +84,6 @@ public class DiceDialog extends JDialog implements ActionListener
 	{
 		this.setVisible(true);
 		
-		return 2;
+		return 1;
 	}
 }
--- src/gui/engine/DicePanel.java
+++ src/gui/engine/DicePanel.java
@@ -1,28 +1,178 @@
 package src.gui.engine;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import java.awt.event.*;
 
 import java.awt.*;
 
+import java.util.Random;
+
 // Panel to draw the dice themselves
+// performs random drawing of dice before displaying result
 public class DicePanel extends JPanel implements ActionListener
 {
-	public DicePanel()
+	// so we can pass control pack to the button
+	private DiceDialog parent;
+	
+	// dice rolls to show at end
+	private int d1, d2;
+	
+	// variables to track random dice and proper rolls at the end
+	private int r1, r2;
+	
+	// how long between ticks (ms)
+	private int tick = 100;
+	
+	// how many ticks before we stop
+	private int tick_length = 9;
+	
+	// count of ticks
+	private int tick_count = 0;
+	
+	// so we draw what the result is
+	private boolean done = false;
+	
+	// random
+	private Random rng;
+	
+	// timer
+	private Timer timer;
+	
+	public DicePanel(DiceDialog parent, int d1, int d2, Dimension size)
 	{
+		this.setPreferredSize(size);
 		
+		this.parent = parent;
+		this.d1 = d1;
+		this.d2 = d2;
+		
+		timer = new Timer(tick, this);
+		
+		rng = new Random();
+		
+		r1 = roll();
+		r2 = roll();
+		
+		timer.start();
+	}
+	
+	public int roll()
+	{
+		return rng.nextInt(6) + 1;
 	}
 	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
 		
+		Dimension dim = this.getSize();
 		
+		int width = (int)dim.getWidth();
+		int height = (int)dim.getHeight();
+		
+		int half_width = width/2;
+		
+		Graphics2D g2D = (Graphics2D)g;
+		
+		int dis_half_width = 3*(half_width/2)/4;
+		int dis_half_height = 3*(height/2)/4;
+		
+		draw_dice_roll(g2D, half_width/2, height/2, dis_half_width, dis_half_height, r1, Color.YELLOW, Color.RED);
+		draw_dice_roll(g2D, 3*half_width/2, height/2, dis_half_width, dis_half_height, r2, Color.RED, Color.YELLOW);
+		
+		if (done)
+		{
+			g.setColor(Color.BLACK);
+			int font = height/8;
+			g.setFont(new Font("Comic Sans MS", Font.BOLD, font));
+			String numb = (r1 + r2) + "";
+			g2D.drawString((r1 + r2) + "", half_width - numb.length()*font/2 + 10, 7*height/8 + font - 10);
+		}
+	}
+	
+	// draws a dice roll onto the screen
+	// g - graphics
+	// x0,y0 - mid point of the roll
+	// width - half-width of rectangle
+	// height - half-height of rectangle
+	// number - number from 1-6 (does nothing if its bigger), amount of dots to draw
+	// back_col - background color of rectangle behind dots
+	// dot_col - color of the dots
+	public void draw_dice_roll(Graphics2D g, int x0, int y0, int width, int height, int number, Color back_col, Color dot_col)
+	{
+		g.setColor(back_col);
+		
+		g.fillRect(x0 - width, y0 - height, 2*width, 2*height);
+		
+		g.setColor(dot_col);
+		
+		// return early if invalid number
+		if (number < 1 || number > 6)
+			return;
+		
+		int r = width/5;
+		if (height < width)
+			r = height/5;
+	
+		if (number % 2 == 1) // odd draw middle dot
+		{
+			g.fillOval(x0 - r, y0 - r, 2*r, 2*r);
+		}
+		
+		if (number >= 2)
+		{
+			g.fillOval(x0 - width/2 - r, y0 - height/2 - r, 2*r, 2*r);
+			g.fillOval(x0 + width/2 - r, y0 + height/2 - r, 2*r, 2*r);
+		}
+		
+		if (number >= 4)
+		{
+			g.fillOval(x0 - width/2 - r, y0 + height/2 - r, 2*r, 2*r);
+			g.fillOval(x0 + width/2 - r, y0 - height/2 - r, 2*r, 2*r);
+		}
+		
+		if (number == 6)
+		{
+			g.fillOval(x0 - width/2 - r, y0 - r, 2*r, 2*r);
+			g.fillOval(x0 + width/2 - r, y0 - r, 2*r, 2*r);
+		}
 	}
 	
 	public void actionPerformed(ActionEvent e)
 	{
+		if (tick_count < tick_length)
+		{
+			tick_count++;
+			
+			// if its the last one make it so the random rolls cant be the same as our actual rolls
+			// just to improve clarity
+			if (tick_count == tick_length - 1)
+			{
+				r1 = rng.nextInt(5) + 1;
+				if (r1 >= d1)
+					r1++;
+				
+				r2 = rng.nextInt(5) + 1;
+				if (r2 >= d2)
+					r2++;
+			}
+			else
+			{
+				r1 = roll();
+				r2 = roll();
+			}
+		}
+		else
+		{
+			r1 = d1;
+			r2 = d2;
+			done = true;
+			timer.stop();
+			parent.enable_ok();
+		}
 		
+		repaint();
 	}
 }
